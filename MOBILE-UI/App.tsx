58b235ddf24d36d41a055aeda1851563b40a7123
Nicolas-Foster-davinci <89162407+Nicolas-Foster-davinci@users.noreply.github.com>
2024-04-03 12:06:10 -0300

    Update App.tsx

--- MOBILE-UI/App.tsx
+++ MOBILE-UI/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
-import Header from './src/common/Header'; // Asegúrate de tener la ruta correcta a Header.tsx
+import Header from './src/common/Header'; 
 
 const App: React.FC = () => {
   return (
     <View>
       <Header />
-      {/* Otros componentes */}
     </View>
   );
 }
